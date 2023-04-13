a435eaea932942541c05590bcf330b73dc05774e
sydneyyyyyyy <qwer1478792@gmail.com>
2023-04-13 18:12:50 +0900

    Feat: 프로젝트 초기 설정

---
+++ src/main/java/com/fittrio/fitday/FitdayApplication.java
@@ -0,0 +1,13 @@
+package com.fittrio.fitday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitdayApplication.class, args);
+	}
+
+}
