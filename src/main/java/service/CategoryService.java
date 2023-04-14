2ebefa134802877bca146f05cdad1929ec2864f5
thfastang401 <thfastang401@naver.com>
2023-04-14 16:46:56 +0900

    Feat: 패키지 및 매핑파일 생성

---
+++ src/main/java/service/CategoryService.java
@@ -0,0 +1,28 @@
+package service;
+
+import com.starters.ityogurt.dto.CategoryDTO;
+import com.starters.ityogurt.dto.UserDTO;
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategoryList();
+    CategoryDTO getCategoryByCategorySeq(String categorySeq);
+
+    int countAllSub();
+    int countAllCategory();
+
+    List<CategoryDTO> getCategoryByType(String type, String typeValue);
+    CategoryDTO getCategoryByAllType(CategoryDTO categoryDTO);
+
+    void insertCategory(CategoryDTO categoryDTO);
+
+
+    int findWeakCategoryByCategorySeq(int userSeq);
+
+    CategoryDTO getCategoryByUserSeq(int userSeq);
+	int getCategoryBySub(String sub);
+	List<CategoryDTO> getMiddleCategoryGroup();
+    
+
+}
