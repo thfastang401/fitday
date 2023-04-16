f511abd9df3067fb999c0dc4b3eeb8324c09b914
HK <sweetapplepie153@gmail.com>
2023-04-16 17:24:20 +0900

    Feat : login 페이지

--- src/main/java/com/fittrio/fitday/config/SecurityConfig.java
+++ src/main/java/com/fittrio/fitday/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -14,7 +16,17 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers(
                 new AntPathRequestMatcher("/**")).permitAll()
-                ;
+	        .and()
+	        .formLogin()
+	        .loginPage("/user/login")
+	        .defaultSuccessUrl("/")
+             ;
         return http.build();
     }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
 }
--- src/main/java/com/fittrio/fitday/controller/UserController.java
+++ src/main/java/com/fittrio/fitday/controller/UserController.java
@@ -4,6 +4,7 @@ import com.fittrio.fitday.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
@@ -11,4 +12,10 @@ public class UserController {
     @Qualifier("userService")
     UserService service;
 
+    
+    @GetMapping("/user/login")
+    public String login() {
+        return "loginForm";
+    }
+    
 }
