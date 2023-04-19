d053383a53cdf9490014741da8de4abb084107ba
HK <sweetapplepie153@gmail.com>
2023-04-19 17:57:44 +0900

    Feat : 유저 정보 표시

--- src/main/java/com/fittrio/fitday/controller/MainController.java
+++ src/main/java/com/fittrio/fitday/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.fittrio.fitday.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	@GetMapping("/")
-    public String main() {
+    public String main(@AuthenticationPrincipal User user, Model model) {
+		if(user != null) {
+			model.addAttribute("loginId", user.getUsername());
+			model.addAttribute("loginRoles", user.getAuthorities());
+		}
         return "main";
     }
 	
